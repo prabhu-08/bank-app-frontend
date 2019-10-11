dde2915a13fe43ac1905e11eb8a05e59e72306d2
Prabhudev Avarasang <prabhudev.avarasang@wdc.com>
2019-10-11 14:53:14 +0530

    added labels and other things

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,34 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
+    <v-layout row wrap justify-end>
+      <v-spacer></v-spacer>
+      <v-flex xs3>
+        <v-select
+          :items="items"
+          label="Duration"
+          v-model="duration"
+          @change="fetchData"
+          item-value="id"
+          item-text="display"
+          class="ma-3"
+        ></v-select>
+      </v-flex>
+      <v-btn text icon color="green" class="ma-6" v-on:click="refresh">
+        <v-icon>mdi-cached</v-icon>
+      </v-btn>
+    </v-layout>
+    <v-layout text-center wrap>
       <v-flex xs12>
-        <v-col
-          class="d-flex"
-          cols="12"
-          sm="6"
-        >
-          <v-select
-            :items="items"
-            label="Duration"
-            solo
-            v-model="duration"
-            @change="fetchData"
-            item-value="id"
-            item-text="display"
-          ></v-select>
-        </v-col>
-        <highcharts
-          class="chart"
-          :options="chartOptions"
-          :updateArgs="updateArgs"
-        ></highcharts>
+        <highcharts class="chart" :options="chartOptions" :updateArgs="updateArgs"></highcharts>
       </v-flex>
     </v-layout>
+    <v-dialog v-model="loading" fullscreen full-width>
+      <v-container fluid fill-height style="background-color: rgba(255, 255, 255, 0.5);">
+        <v-layout justify-center align-center>
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        </v-layout>
+      </v-container>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -44,6 +46,7 @@ export default {
     return {
       duration: "",
       updateArgs: [true, true, { duration: 1000 }],
+      loading: false,
 
       items: [
         { id: "week", display: "1 Week" },
@@ -64,66 +67,70 @@ export default {
           tickInterval: 1000 * 60 * 15,
           labels: {
             formatter: function() {
-              console.log(this.value);
               return Highcharts.dateFormat("%H:%M", this.value);
             }
           }
         },
+        yAxis: {
+          title: {
+            text: "Average Transcation Amount"
+          },
+          labels: {
+            format: "{value:.2f}"
+          }
+        },
         plotOptions: {
+          bubble: {
+            tooltip: {
+              pointFormat: "Avg Transcation amount {point.y}"
+            }
+          },
           series: {
             pointInterval: 1000 * 60 * 15 //data every 15 minutes
           }
         },
         series: [
           {
-            data: [{ x: 15706962000001, y: 368 }]
+            data: [],
+            name: "Transcation data"
           }
         ]
       }
     };
   },
-  created() {},
+  created() {
+    this.duration = "1hour";
+    this.fetchData();
+  },
   watch: {},
   methods: {
     formatData: function(timeData) {
       let finalData = [];
       for (var data of timeData) {
-        console.log(data);
         finalData.push({ x: data["_id"]["$date"], y: data["total_amount"] });
-        // tmp_data.push(data["total_amount"]);
       }
-      // finalData.push(tmp_data);
-      console.log(finalData);
       let series = { data: finalData };
       this.chartOptions.series = [series];
     },
     fetchData: function() {
+      this.loading = true;
       axios
         .get("api/data?duration=" + this.duration)
         .then(response => {
-          console.log(response.data);
+          this.loading = false;
           this.formatData(response.data);
         })
         .catch(error => {
-          console.log(error);
+          this.loading = false;
         });
+    },
+    refresh: function() {
+      this.duration = "week";
+      this.fetchData();
     }
   }
 };
 </script>
 
 <style scoped>
-/* input[type="color"]::-webkit-color-swatch-wrapper {
-  padding: 0;
-}
-#colorPicker {
-  border: 0;
-  padding: 0;
-  margin: 0;
-  width: 30px;
-  height: 30px;
-}
-.numberInput {
-  width: 30px;
-} */
 </style>
